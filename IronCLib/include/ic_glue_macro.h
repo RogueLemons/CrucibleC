8edbc03991623f9d62b7f28c54a1dd49b11258b0
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2026-04-21 14:02:07 +0200

    Update ic_glue_macro

--- IronCLib/include/ic_glue_macro.h
+++ IronCLib/include/ic_glue_macro.h
@@ -7,6 +7,9 @@
 #define IC_GLUE4(a, b, c, d) IC_GLUE(IC_GLUE3(a, b, c), d)
 #define IC_GLUE5(a, b, c, d, e) IC_GLUE(IC_GLUE4(a, b, c, d), e)
 #define IC_GLUE6(a, b, c, d, e, f) IC_GLUE(IC_GLUE5(a, b, c, d, e), f)
+#define IC_GLUE7(a, b, c, d, e, f, g) IC_GLUE(IC_GLUE6(a, b, c, d, e, f), g)
+#define IC_GLUE8(a, b, c, d, e, f, g, h) IC_GLUE(IC_GLUE7(a, b, c, d, e, f, g), h)
+#define IC_GLUE9(a, b, c, d, e, f, g, h, i) IC_GLUE(IC_GLUE8(a, b, c, d, e, f, g, h), i)
 
 
 #endif // IC_GLUE_MACRO_H
