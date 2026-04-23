ed7792568e22ca9526cc0d3cf4da1c4e1c6e9b48
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2026-04-23 23:10:12 +0200

    asd

--- IronCLib/ironclib/ic.h
+++ IronCLib/ironclib/ic.h
@@ -6,10 +6,10 @@
 #include "ic_static_assert.h"
 #include "ic_bounded_loop.h"
 // Internally dependent on the above (no circular dependencies)
+#include "ic_memory.h"
 #include "ic_num_cast.h"
 #include "ic_typenum.h"
 #include "ic_result.h"
-#include "ic_memory.h"
 #include "ic_opaque_storage.h"
 
 #endif // IRON_C_FULL_H
